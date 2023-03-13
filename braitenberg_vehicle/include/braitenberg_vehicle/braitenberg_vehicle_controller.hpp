1d69152634348a44a8b0ddc7286b5bd3a538d285
Masaya Kataoka <ms.kataoka@gmail.com>
2023-03-14 00:37:06 +0900

    add callback functions
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- braitenberg_vehicle/include/braitenberg_vehicle/braitenberg_vehicle_controller.hpp
+++ braitenberg_vehicle/include/braitenberg_vehicle/braitenberg_vehicle_controller.hpp
@@ -5,6 +5,8 @@
 #include <geometry_msgs/msg/twist.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/laser_scan.hpp>
+#include <optional>
+#include <mutex>
 
 #include "braitenberg_vehicle/visibility_control.h"
 
@@ -20,24 +22,44 @@ public:
   virtual ~BraitenbergVehicleController();
 
 private:
+  // パラメータを取得するためのテンプレート関数、第一引数はパラメータ名、第二引数はデフォルト値
   template<typename T>
   auto get_parameter(const std::string & name, const T & default_value) -> T
   {
+    // 取得したいパラメータが宣言済みかを確認
     if (!has_parameter(name)) {
+      // 宣言済み出なかった場合、宣言しつつデフォルト値をセット
       declare_parameter(name, default_value);
     }
     T value;
+    // パラメータの値を取得
     get_parameter(name, value);
     return value;
   }
-  void scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr scan);
   // cmd_velに対して速度司令を出すPublisher
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr twist_pub_;
   // /scanトピック経由でLiDARによるスキャン情報を取得するsubscriber
   rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr scan_sub_;
+  // scanトピック経由で点群を受け取るコールバック関数
+  void scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr scan);
   // ゴール地点を受信するsubscription
   rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr goal_pose_sub_;
+  // ゴール地点を受信するコールバック関数
+  void goal_pose_callback(const geometry_msgs::msg::PoseStamped::SharedPtr pose);
+  // ゴール地点を記録するメンバ変数
+  // ゴール地点受信前の状態を表現するためstd::optionalを用いて無効値を表現
+  // https://cpprefjp.github.io/reference/optional/optional.html
+  std::optional<geometry_msgs::msg::Pose> goal_pose_;
+  // ホイールの半径を記録するメンバ変数
   double wheel_radius_;
+  // ベースリンクのframe_idを記録するメンバ変数
+  std::string base_link_frame_id_;
+  // 制御コマンド更新のためのタイマー
+  rclcpp::TimerBase::SharedPtr timer_;
+  // 一定時間ごとに刻まれるタイマーのコールバック
+  void timer_callback();
+  // データ同期用のmutex
+  std::mutex mutex_;
 };
 
 }  // namespace braitenberg_vehicle
--- braitenberg_vehicle/src/braitenberg_vehicle_controller.cpp
+++ braitenberg_vehicle/src/braitenberg_vehicle_controller.cpp
@@ -6,16 +6,48 @@ namespace braitenberg_vehicle
 BraitenbergVehicleController::BraitenbergVehicleController(const rclcpp::NodeOptions & options)
 : rclcpp::Node("braitenberg_vehicle_controller", options)
 {
+  // "wheel_radius"パラメータを読み込みメンバ変数にセット
   wheel_radius_ = get_parameter("wheel_radius", 0.033);
+  // "base_link_frame_id"パラメータを読み込み、メンバ変数にセット
+  base_link_frame_id_ = get_parameter("base_link_frame_id", std::string("base_link"));
+  // publisherを作る関数、テンプレート引数はメッセージ型、第一引数はtopic名、第二引数は送信バッファサイズ
   twist_pub_ = create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 1);
+  // subscriptionを作る関数、テンプレート引数はメッセージ型、第一引数はtopic名、第二引数は受信バッファサイズ、第三引数にコールバック関数を登録
   scan_sub_ = create_subscription<sensor_msgs::msg::LaserScan>(
     "/scan", 1, [this](const sensor_msgs::msg::LaserScan::SharedPtr scan) {scan_callback(scan);});
+  // subscriptionを作る関数、テンプレート引数はメッセージ型、第一引数はtopic名、第二引数は受信バッファサイズ、第三引数にコールバック関数を登録
+  goal_pose_sub_ = create_subscription<geometry_msgs::msg::PoseStamped>(
+    "/goal_pose", 1, [this](const geometry_msgs::msg::PoseStamped::SharedPtr pose) {
+      goal_pose_callback(pose);
+    });
 }
 
 BraitenbergVehicleController::~BraitenbergVehicleController() {}
 
 void BraitenbergVehicleController::scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr scan)
 {
+  mutex_.lock();
+  mutex_.unlock();
 }
 
+void BraitenbergVehicleController::goal_pose_callback(
+  const geometry_msgs::msg::PoseStamped::SharedPtr pose)
+{
+  mutex_.lock();
+  // 受信したゴール姿勢のframe_id(座標系の名前)が適切なものかを確認
+  if (pose->header.frame_id == base_link_frame_id_) {
+    // 適切な場合は、goal_pose_に受信したposeを代入
+    goal_pose_ = pose->pose;
+  } else {
+    // 不適切な場合は、goal_pose_に無効値を表現するstd::nulloptをセット
+    goal_pose_ = std::nullopt;
+  }
+  mutex_.unlock();
+}
+
+void BraitenbergVehicleController::timer_callback()
+{
+  mutex_.lock();
+  mutex_.unlock();
+}
 }  // namespace braitenberg_vehicle
